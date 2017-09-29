a7f61b687f1f68897075224462ff40f25e1ae137
sahil <kanu.makkar.0172@gmail.com>
2017-09-29 13:08:45 +0530

    upcode

---
+++ devdojo/calculator/src/routes.php
@@ -0,0 +1,4 @@
+<?php
+
+Route::get('add/{a}/{b}', 'Devdojo\Calculator\CalculatorController@add');
+Route::get('subtract/{a}/{b}', 'Devdojo\Calculator\CalculatorController@subtract');
---
+++ devdojo/calculator/src/views/add.blade.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<title>Calculator</title>
+</head>
+<body>
+	<h1 style="text-align:center">
+		Your Result 
+		<span style="font-weight:normal">{{ $result }}</span>
+	</h1>
+</body>
+</html>
